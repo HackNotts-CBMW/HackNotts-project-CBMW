04ad8ab50e26ce7e66a0c028c242f52602b320b9
EsCharl <63916811+EsCharl@users.noreply.github.com>
2023-02-11 21:09:11 +0000

    too good to go

--- backend/RUNME.py
+++ backend/RUNME.py
@@ -29,6 +29,11 @@ def login():
 def findAccount():
     backend.findTransactionCategory.findTotalCategorySpent()
 
+
+@app.route('/find')
+def findFoodDeals():
+    backend.findTransactionCategory.findTotalCategorySpent()
+
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
 		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
